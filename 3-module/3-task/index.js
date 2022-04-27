c0c066762fb11a9676699d3e7327f6c2e7eb9c00
kreyttsfeldt <antipov.in@gmail.com>
2022-04-27 13:31:17 +0300

    Сделал задания модуля 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,10 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  return arr[0]+arr.slice(1).map( (elem) => {
+    if (elem.length !==0){
+      return elem[0].toUpperCase()+elem.slice(1);
+    }
+    return elem;
+  }).join('');
 }
+
